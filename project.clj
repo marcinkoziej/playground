537f0eafd0da44d93db800ffa80f9d518e029a45
Marcin Koziej <marcin@lolownia.org>
2012-03-12 07:37:26 +0100

    first commit

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject playground "0.1.0-SNAPSHOT"
+  :description "Overtone test"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [overtone "0.6.0"]]
+  ;; :profiles {
+  ;;            :dev {:dependencies
+  ;;                  [[lein-swank "1.4.0"]
+  ;;                   [cdt/cdt "1.2.6.2"]
+                     
+  ;;                   ]}
+  ;;            }
+
+  
+  )
---
+++ src/playground/test.clj
@@ -0,0 +1,5 @@
+(ns playground.test
+  (:use overtone.live
+        ))
+
+
